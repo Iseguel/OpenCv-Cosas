9be3388e369fe4eac10550189aedef8e757fcd70
Ismael Seguel <seguel.ismael@gmail.com>
2017-08-15 22:41:36 -0300

    Update install-opencv-2.4.13-Raspberry.sh

--- install-opencv-2.4.13-Raspberry.sh
+++ install-opencv-2.4.13-Raspberry.sh
@@ -1,13 +1,13 @@
 # install dependencies
 echo "instalacion de dependencias"
 sudo apt-get update
-sudo apt-get install  build-essential
-sudo apt-get install  cmake
-sudo apt-get install  libgtk2.0-dev
-sudo apt-get install  pkg-config
-sudo apt-get install  python-numpy python-dev
-sudo apt-get install  libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install  libjpeg-dev libpng-dev libtiff-dev libjasper-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+sudo apt-get install -y libgtk2.0-dev
+sudo apt-get install -y pkg-config
+sudo apt-get install -y python-numpy python-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev libjasper-dev
  
 sudo apt-get -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libqt4-dev libgtk2.0-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
  
